d838a0d83efcf8673d86293464b61218091bb390
Wolkvicz <114021622+Wolkvicz@users.noreply.github.com>
2026-05-21 16:35:56 -0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-require __DIR__.'/admin.php';
-require __DIR__.'/Candidato.php';
-
 Route::get('/', function () {
-    return view('welcome');
-    // Rota de teste
-Route::get('/painel-admin', function () {
-    return "Se você está vendo isso, você é um Administrador!";
-})->middleware(['auth', 'admin']);
+    return view('arquivo');
+});
+
+Route::get('/perfil', function () {
+    return view('perfil');
 });

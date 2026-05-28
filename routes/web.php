e86e2d172e2cec03e76a4cbe3461fe744c8de203
Renan Ramlov <76700320+renanramlov@users.noreply.github.com>
2026-05-28 20:10:19 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+#candidato
 Route::get('/', function () {
     return view('arquivo');
 });
-
+#candidato
 Route::get('/perfil', function () {
     return view('perfil');
 });
